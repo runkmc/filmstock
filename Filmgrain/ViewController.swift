fc5648fa0825db9b327f9037bb34e48ff7fd405b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-09-23 14:00:35 -0700

    Initial Commit

---
+++ Filmgrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filmgrain
+//
+//  Created by Kevin McGladdery on 9/23/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
