b037f85305fc3a6176d686dd719e5bcfb0c4d7bb
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-10-01 11:12:45 -0700

    Add cancel unwinds

--- CamerasViewController.swift
+++ CamerasViewController.swift
@@ -12,19 +12,16 @@ class CamerasViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwindToTable(segue: UIStoryboardSegue) {
+      
+    }
+  
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
