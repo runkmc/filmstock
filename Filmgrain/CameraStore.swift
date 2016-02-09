cf3b9c00472c52e7b0e2ba36ab4c9231e61087a2
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-08 21:03:57 -0800

    Fix indentation

--- Filmgrain/CameraStore.swift
+++ Filmgrain/CameraStore.swift
@@ -9,22 +9,22 @@
 import Foundation
 
 class CameraStore {
-  
-  static var store = [Camera]()
-  
-  static func path() -> String {
-    let dirs = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-    return "\(dirs[0])items.archive"
-  }
-  
-  static func save() -> Bool {
-    return NSKeyedArchiver.archiveRootObject(self.store, toFile: self.path())
-  }
-  
-  static func load() {
-   guard let s = NSKeyedUnarchiver.unarchiveObjectWithFile(self.path()) as? [Camera]
-    else { return }
-    
-    self.store = s
-  }
+
+	static var store = [Camera]()
+
+	static func path() -> String {
+		let dirs = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+		return "\(dirs[0])items.archive"
+	}
+
+	static func save() -> Bool {
+		return NSKeyedArchiver.archiveRootObject(self.store, toFile: self.path())
+	}
+
+	static func load() {
+		guard let s = NSKeyedUnarchiver.unarchiveObjectWithFile(self.path()) as? [Camera]
+		else { return }
+
+		self.store = s
+	}
 }
