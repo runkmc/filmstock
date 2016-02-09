ccd2041f1bfa8b75e5cad4a4936258d1739522ec
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-09 13:57:23 -0800

    Refactor into smaller functions

--- Filmgrain/LoadCameraController.swift
+++ Filmgrain/LoadCameraController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class LoadCameraController: UIViewController {
 
-	var camera: Camera?
+	var camera: Camera!
 	@IBOutlet weak var cameraName: UILabel!
 	@IBOutlet weak var notesField: UITextView!
 	@IBOutlet weak var cancelButton: UIButton!
@@ -18,29 +18,29 @@ class LoadCameraController: UIViewController {
 
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		let button = sender as! UIButton
-
 		if button.titleForState(.Normal) == "Load Camera" {
-			camera?.loaded = true
-            camera?.notes = notesField.text ?? ""
+			camera.loaded = true
+            camera.notes = notesField.text ?? ""
 		}
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		cameraName.text = camera?.name
-		loadButton.layer.borderWidth = 1
-		loadButton.layer.cornerRadius = 22
-		loadButton.layer.borderColor = UIColor.init(red:0.086, green:0.651, blue:0.341, alpha:1.000).CGColor
-		cancelButton.layer.borderWidth = 1
-		cancelButton.layer.cornerRadius = 22
-		cancelButton.layer.borderColor = UIColor.init(red:0.804, green:0.000, blue:0.078, alpha:1.000).CGColor
-
+		cameraName.text = camera.name
+        styleButton(loadButton, withColor: UIColor.init(red:0.086, green:0.651, blue:0.341, alpha:1.000))
+        styleButton(cancelButton, withColor: UIColor.init(red:0.804, green:0.000, blue:0.078, alpha:1.000))
 		let tapper = UITapGestureRecognizer.init(target: self, action: "dismiss")
 		tapper.cancelsTouchesInView = false
 		self.view.addGestureRecognizer(tapper)
 
 		// Do any additional setup after loading the view.
 	}
+    
+    func styleButton(button:UIButton, withColor color:UIColor) {
+		button.layer.borderWidth = 1
+		button.layer.cornerRadius = 22
+		button.layer.borderColor = color.CGColor
+    }
 
 	func dismiss() {
 		self.view.endEditing(true)
@@ -50,16 +50,4 @@ class LoadCameraController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
-	/*
-	// MARK: - Navigation
-
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-	// Get the new view controller using segue.destinationViewController.
-	// Pass the selected object to the new view controller.
-	}
-	*/
-
 }
