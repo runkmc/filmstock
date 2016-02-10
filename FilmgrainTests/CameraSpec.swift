f628a8dae1f8e25219479d6a4a893ccb460d246c
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-10 10:47:37 -0800

    Add CameraSpec file

---
+++ FilmgrainTests/CameraSpec.swift
@@ -0,0 +1,24 @@
+//
+//  CameraSpec.swift
+//  Filmstock
+//
+//  Created by Kevin McGladdery on 2/10/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+@testable import Filmstock
+
+class CameraSpec: QuickSpec {
+    override func spec() {
+        describe("A Camera") {
+            let cam = Camera(name:"Nikon 1", notes:"Dropped it in a pool", loaded:true)
+            it("has properties") {
+                
+                
+            }
+        }
+    }
+}
