17c266fee603c9de988530e0e517c1cc74da9f34
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-12 11:04:28 -0800

    Remove comments

--- Filmgrain/CameraDetailsController.swift
+++ Filmgrain/CameraDetailsController.swift
@@ -18,13 +18,6 @@ class CameraDetailsController: UITableViewController {
 		super.viewDidLoad()
 		self.nameField.text = camera!.name
 		self.notesField.text = camera!.notes
-
-		// Uncomment the following line to preserve selection between presentations
-		// self.clearsSelectionOnViewWillAppear = false
-
-		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-		// self.navigationItem.rightBarButtonItem = self.editButtonItem()
-
 	}
 
 	override func viewWillDisappear(animated: Bool) {
@@ -45,6 +38,5 @@ class CameraDetailsController: UITableViewController {
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 }
