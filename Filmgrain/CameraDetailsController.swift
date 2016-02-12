70a8e1d092521e1b4b22b4513dea3b8068bcb758
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-12 11:06:14 -0800

    Change optional property to implicitly unwrapped optional

--- Filmgrain/CameraDetailsController.swift
+++ Filmgrain/CameraDetailsController.swift
@@ -12,7 +12,7 @@ class CameraDetailsController: UITableViewController {
 
 	@IBOutlet weak var nameField: UITextField!
 	@IBOutlet weak var notesField: UITextView!
-	var camera: Camera?
+	var camera: Camera!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,14 +22,11 @@ class CameraDetailsController: UITableViewController {
 
 	override func viewWillDisappear(animated: Bool) {
 		super.viewWillDisappear(animated)
-		if nameField.text?.characters.count > 0 {
-			camera!.name = nameField.text!
-		}
-
+		camera.name = nameField.text!
 		if notesField.text.characters.count > 0 {
-			camera!.notes = notesField.text!
+			camera.notes = notesField.text!
 		} else {
-			camera!.notes = ""
+			camera.notes = ""
 		}
 	}
 
